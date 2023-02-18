346449c827a15fe4b0242498d20ca6c4695678d7
Shalaj Wadhwa <2639429w@student.gla.ac.uk>
2023-02-18 23:44:38 +0000

    comments

--- main.py
+++ main.py
@@ -6,17 +6,22 @@ n = len(layout[0]) # total number of columns
 teams = []
 
 def find_team_else_create(i, j):
-    if (type(layout[i-1][j]) == type(str()) and (i - 1) >= 0): # vertically up
+    # check vertically above (up)
+    if (type(layout[i-1][j]) == type(str()) and (i - 1) >= 0): # part of existing team
         index = int(layout[i-1][j])
         teams[index] += 1
         layout[i][j] = str(index)
-    elif (type(layout[i][j-1]) == type(str() and (j - 1) >= 0)): # horizontally left
+    
+    # check horizontally below (left)
+    elif (type(layout[i][j-1]) == type(str() and (j - 1) >= 0)): # part of existing team
         index = int(layout[i][j-1])
         teams[index] += 1
         layout[i][j] = str(index)
+    
+    # no team found
     else:
-        teams.append(1)
-        layout[i][j] = str((len(teams)-1))
+        teams.append(1) # create a new team
+        layout[i][j] = str((len(teams)-1)) # replace layout position with index of team
 
 for i in range(0, m):
     for j in range(0, n):
